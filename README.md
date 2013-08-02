6a85dd6c6a46127a2e1860d7d98b0e2faa0f4b8b
Yasutaka ATARASHI <yakex@cpan.org>
2013-08-02 12:46:20 +0900

    Initial setup.

---
+++ README.md
@@ -0,0 +1,80 @@
+# NAME
+
+Dist::Zilla::PluginBundle::Author::YAKEX - Dist::Zilla configuration the way YAKEX does it
+
+# VERSION
+
+version v0.0.0
+
+# SYNOPSIS
+
+    # in dist.ini
+    [@Author::YAKEX]
+
+# DESCRIPTION
+
+This is a [Dist::Zilla](http://search.cpan.org/perldoc?Dist::Zilla) PluginBundle used by YAKEX's distribution. It is equivalent to the following dist.ini
+
+    [@Basic]
+    
+
+    [NextRelease]
+    filename = Changes
+    
+
+    [@Git]
+    commit_msg = Released as %v%n%n%c
+    tag_format = %v
+    tag_message = Released as %v
+    allow_dirty = Changes
+    allow_dirty = README.md
+    allow_dirty = dist.ini
+    
+
+    [ReadmeAnyFromPod]
+    type = markdown
+    filename = README.md
+    location = root
+    
+
+    [GithubMeta]
+    [MetaJSON]
+    [PodWeaver]
+    [OurPkgVersion]
+    [CheckVersionIncrement]
+    [PodSyntaxTests]
+    [PodCoverageTests]
+    [Test::Perl::Critic]
+    [Test::Compile]
+    [Test::Kwalitee::Extra]
+    arg = !has_example
+    
+
+    [Twitter]
+    url_shortener = none
+    hash_tags = #perl
+    
+
+    [MinimumPerl]
+
+This PluginBundle also supports PluginRemover, so dropping a plugin is as easy as this:
+
+    [@YAKEX]
+    -remove = PluginDontUse
+
+# METHODS
+
+## `configure`
+
+See [Dist::Zilla::Role::PluginBundle::Easy](http://search.cpan.org/perldoc?Dist::Zilla::Role::PluginBundle::Easy)
+
+# AUTHOR
+
+Yasutaka ATARASHI <yakex@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Yasutaka ATARASHI.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/Dist/Zilla/PluginBundle/Author/YAKEX.pm
+++ lib/Dist/Zilla/PluginBundle/Author/YAKEX.pm
@@ -3,6 +3,97 @@ package Dist::Zilla::PluginBundle::Author::YAKEX;
 use strict;
 use warnings;
 
+# ABSTRACT: Dist::Zilla configuration the way YAKEX does it
 # VERSION
 
+use Moose;
+with 'Dist::Zilla::Role::PluginBundle::Easy';
+with 'Dist::Zilla::Role::PluginBundle::PluginRemover';
+
+sub configure
+{
+	my $self = shift;
+	$self->add_bundle('Basic');
+	$self->add_plugins(
+		[ NextRelease => { filename => 'Changes' } ],
+	);
+	$self->add_bundle(Git => {
+		commit_msg => 'Released as %v%n%n%c',
+		tag_format => '%v',
+		tag_message => 'Released as %v',
+		allow_dirty => [ 'Changes', 'README.md', 'dist.ini' ],
+	});
+	$self->add_plugins(
+		[ ReadmeAnyFromPod => {
+			type => 'markdown',
+			filename => 'README.md',
+			location => 'root',
+		} ],
+		qw[
+			GithubMeta MetaJSON PodWeaver OurPkgVersion CheckVersionIncrement MinimumPerl
+			PodSyntaxTests PodCoverageTests Test::Perl::Critic Test::Compile
+		],
+		[ 'Test::Kwalitee::Extra' => { arg => '!has_example' } ],
+		[ Twitter => { url_shortener => 'none', hash_tags => '#perl' } ],
+	);
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
 1;
+__END__
+
+=head1 SYNOPSIS
+
+  # in dist.ini
+  [@Author::YAKEX]
+
+=head1 DESCRIPTION
+
+This is a L<Dist::Zilla> PluginBundle used by YAKEX's distribution. It is equivalent to the following dist.ini
+
+  [@Basic]
+  
+  [NextRelease]
+  filename = Changes
+  
+  [@Git]
+  commit_msg = Released as %v%n%n%c
+  tag_format = %v
+  tag_message = Released as %v
+  allow_dirty = Changes
+  allow_dirty = README.md
+  allow_dirty = dist.ini
+  
+  [ReadmeAnyFromPod]
+  type = markdown
+  filename = README.md
+  location = root
+  
+  [GithubMeta]
+  [MetaJSON]
+  [PodWeaver]
+  [OurPkgVersion]
+  [CheckVersionIncrement]
+  [PodSyntaxTests]
+  [PodCoverageTests]
+  [Test::Perl::Critic]
+  [Test::Compile]
+  [Test::Kwalitee::Extra]
+  arg = !has_example
+  
+  [Twitter]
+  url_shortener = none
+  hash_tags = #perl
+  
+  [MinimumPerl]
+
+This PluginBundle also supports PluginRemover, so dropping a plugin is as easy as this:
+
+   [@YAKEX]
+   -remove = PluginDontUse
+
+=method C<configure>
+
+See L<Dist::Zilla::Role::PluginBundle::Easy>
